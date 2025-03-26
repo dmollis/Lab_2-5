4c079a6753609a1092ce17fd9649ac42647380f0
Maryna_Trush <m.s.trush@student.khai.edu>
2025-03-26 20:58:14 +0200

    Apply patch from branch progr

--- BootMaster_/obj/Debug/net8.0-windows/BudMayster_.AssemblyInfo.cs
+++ BootMaster_/obj/Debug/net8.0-windows/BudMayster_.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudMayster_")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24cddff3673648bc36b5ca16c89b1f25224349ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7c6f4ca1889c82e87dffaed4a5b54610115fe74")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudMayster_")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudMayster_")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
