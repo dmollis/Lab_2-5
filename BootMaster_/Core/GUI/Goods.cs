df454bf495c1bdf85c1d7450e1f15ec8f03a650c
dmollis <Danil.20033449@gmail.com>
2025-05-18 14:18:12 +0300

    team_lid commit

---
+++ BootMaster_/Core/GUI/Goods.cs
@@ -0,0 +1,99 @@
+﻿using BootMaster_;
+using BudMayster_.Core.Classes;
+
+namespace BudMayster_.Core.GUI
+{
+    public partial class Goods : Form
+    {
+        public Goods()
+        {
+            InitializeComponent();
+            контрагентиToolStripMenuItem.Click += (sender, e) => General.контрагентиToolStripMenuItem_Click(sender, e, this);
+            загальнийToolStripMenuItem.Click += (sender, e) => General.загальнийToolStripMenuItem_Click(sender, e, this);
+            вихідToolStripMenuItem.Click += (sender, e) => General.вихідToolStripMenuItem_Click(sender, e);
+            btn_supp.Click += (sender, e) => General.btn_supp_Click(sender, e, this);
+        }
+
+        private void Goods_Load(object sender, EventArgs e)
+        {
+            Scroll();
+            LoadCategories();
+            LoadCategoriesMenu();
+        }
+
+        private void btn_knauf_Click(object sender, EventArgs e)
+        {
+            General.previousLocation = General.GetLocation(this);
+            KNAYF knaufForm = new KNAYF()
+            {
+                StartPosition = FormStartPosition.Manual,
+                Location = General.previousLocation
+            };
+            knaufForm.Show();
+            this.Hide();
+        }
+
+        private void btnMenu1_Click(object sender, EventArgs e)
+        {
+            General.previousLocation = General.GetLocation(this);
+            KNAYF knaufForm = new KNAYF()
+            {
+                StartPosition = FormStartPosition.Manual,
+                Location = General.previousLocation
+            };
+            knaufForm.Show();
+            this.Hide();
+        }
+
+        private void Scroll()
+        {
+            panel2.AutoScroll = true;
+            vScrollBar1.Visible = true;
+            panel2.Height = 683;
+            vScrollBar1.Maximum = panel2.Height;
+            vScrollBar1.LargeChange = 50;
+        }
+
+        public void LoadCategoriesMenu()
+        {
+            var connection = new DatabaseConnection(Constants.Instance.connection);
+            List<string> categoriesMenu = Categories.GetCategories(connection);
+
+            for (int i = 0; i < categoriesMenu.Count && i < 12; i++)
+            {
+                var button = this.Controls.Find($"btnMenu{i + 1}", true).FirstOrDefault() as System.Windows.Forms.Button;
+                if (button != null)
+                {
+                    button.Text = categoriesMenu[i];
+                }
+            }
+        }
+
+        public void LoadCategories()
+        {
+            var connection = new DatabaseConnection(Constants.Instance.connection);
+            List<string> categories = Categories.GetCategories(connection);
+
+            for (int i = 0; i < categories.Count && i < 12; i++)
+            {
+                var button = this.Controls.Find($"btnСateg{i + 1}", true).FirstOrDefault() as System.Windows.Forms.Button;
+                if (button != null)
+                {
+                    button.Text = categories[i];
+                }
+            }
+        }
+
+        private void btn_supp_Click(object sender, EventArgs e)
+        {
+            General.previousLocation = General.GetLocation(this);
+            Kontragents kontragentsfForm = new Kontragents()
+            {
+                StartPosition = FormStartPosition.Manual,
+                Location = General.previousLocation
+            };
+            kontragentsfForm.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ BootMaster_/Core/GUI/Kontragents.cs
@@ -0,0 +1,77 @@
+﻿using BootMaster_;
+using BudMayster_.Core.Classes;
+
+namespace BudMayster_.Core.GUI
+{
+    public partial class Kontragents : Form
+    {
+        public Kontragents()
+        {
+            InitializeComponent();
+            товариToolStripMenuItem.Click += (sender, e) => General.товариToolStripMenuItem_Click(sender, e, this);
+            загальнийToolStripMenuItem.Click += (sender, e) => General.загальнийToolStripMenuItem_Click(sender, e, this);
+            вихідToolStripMenuItem.Click += (sender, e) => General.вихідToolStripMenuItem_Click(sender, e);
+            btn_goods.Click += (sender, e) => General.btn_goods_Click(sender, e, this);
+        }
+
+        private void btnKontr1_Click(object sender, EventArgs e)
+        {
+            General.previousLocation = General.GetLocation(this);
+            Suppliers suppForm = new Suppliers()
+            {
+                StartPosition = FormStartPosition.Manual,
+                Location = General.previousLocation
+            };
+            suppForm.Show();
+            this.Hide();
+        }
+
+        private void btnMenu1_Click(object sender, EventArgs e)
+        {
+            General.previousLocation = General.GetLocation(this);
+            Suppliers suppForm = new Suppliers()
+            {
+                StartPosition = FormStartPosition.Manual,
+                Location = General.previousLocation
+            };
+            suppForm.Show();
+            this.Hide();
+        }
+
+        private void Suppliers_Load(object sender, EventArgs e)
+        {
+            LoadKontr();
+            LoadKontrMenu();
+        }
+
+        public void LoadKontrMenu()
+        {
+            var connection = new DatabaseConnection(Constants.Instance.connection);
+            List<string> kontrMenu = Categories.GetKontr(connection);
+
+            for (int i = 0; i < kontrMenu.Count && i < 3; i++)
+            {
+                var button = this.Controls.Find($"btnMenu{i + 1}", true).FirstOrDefault() as Button;
+                if (button != null)
+                {
+                    button.Text = kontrMenu[i];
+                }
+            }
+        }
+
+        public void LoadKontr()
+        {
+            var connection = new DatabaseConnection(Constants.Instance.connection);
+            List<string> categories = Categories.GetKontr(connection);
+
+            for (int i = 0; i < categories.Count && i < 3; i++)
+            {
+                var button = this.Controls.Find($"btnKontr{i + 1}", true).FirstOrDefault() as Button;
+                if (button != null)
+                {
+                    button.Text = categories[i];
+                }
+            }
+        }
+    }
+}
